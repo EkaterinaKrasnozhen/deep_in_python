4ffabb4bdba5935cdf699c490f9669c81b5554ac
kate <01redcat@mail.ru>
2023-06-26 14:54:42 +0300

    загадки

--- Seminar6/Task2/Main.py
+++ Seminar6/Task2/Main.py
@@ -2,7 +2,7 @@ import Task2_zagadki as zd
 
 txt = 'сто одежек и все без застежек'
 list_ans = 'капуста'
-ans, num = input(f'Отгадайте загадку {txt}. Введите от и количество попыток через пробел:').split(' ')
+ans, num = input(f'Отгадайте загадку {txt}. Введите от и количество попыток через пробел:').lower().split(' ')
 #print(ans, num)
 res = zd.get_info(txt, list_ans, ans, int(num))
 print(res)
