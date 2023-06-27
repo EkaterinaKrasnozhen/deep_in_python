b200d0f0e2e5fb01bc429e4687ee74b0cf489ead
kate <01redcat@mail.ru>
2023-06-27 20:12:14 +0300

    files

--- Lection7/Ex1.py
+++ Lection7/Ex1.py
@@ -70,8 +70,51 @@
 #         #print(line[:-1]) # тоже убирает посл символ переноса
 #         print(line.replace('\n', ''))# тоже
         
+# text = ' see you next time.'
+# with open('new_text_data.txt', 'a', encoding='utf-8') as f: # append
+#     res = f.write(text)# 19 ск байт информ было записано
+#     print(f'{res=}\n{len(text)=}')
+    
+text = ['hello', 'my name', 'is']
 
+#write
+# with open('new_text_data.txt', 'a', encoding='utf-8') as f:
+#     for line in text:
+#         res = f.write(f'{line}\n') # с переносом на новую строку
+#         print(f'{res=}\n{len(line)=}') 
 
+#writelines
+# with open('new_text_data.txt', 'a', encoding='utf-8') as f:
+#     for line in text:
+#         res = f.writelines('\n'.join(text)) # между каждой из строчек /n, не возвращает кол-во информации res=
+
+#print
+# with open('new_text_data.txt', 'a', encoding='utf-8') as f:
+#     for line in text:
+#         print(line, file=f) #добавяляет переход на новую строку
+
+# выделение print
+# with open('new_text_data.txt', 'a', encoding='utf-8') as f:
+#     for line in text:
+#         print(line, end='***\n##', file=f)# переопредили, начало *** в конце ##
+        
+# f.tell() - где курсов
+# with open('new_text_data.txt', 'w', encoding='utf-8') as f: #стерерть и записать
+#     print(f.tell()) #0
+#     for line in text:
+#         f.write(f'{line}\n')
+#         print(f.tell())# 7, 16, 20
+#     print(f.tell())# 20
+
+# seek(offset, whence=0) смещение от опорной точки, whence0 отсчет от начала файла, когда текст всегда 0, либо 2 - конец, 1 текущая (байт)
+last = before = 0
+with open('new_text_data2.txt', 'r+', encoding='utf-8') as f: #читать и записать
+    while line := f.readline():
+        last, before = f.tell(), last
+        print(f'{last=}, {before=}') #last=7, before=0
+    print(f'{last=}, {before=}')#last=16, before=7, last=18, before=16
+    print(f'{f.seek(before, 0)=}')#f.seek(before, 0)=16 - поставили курсов в нужное место и перезаписали
+    f.write('\n'.join(text))
 
     
 
