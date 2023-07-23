d58f6b42f182caae93f398a21c4424d9e8477d4e
kate <01redcat@mail.ru>
2023-07-23 11:43:28 +0300

    doctest

--- Lection14/example.py
+++ Lection14/example.py
@@ -1,13 +1,18 @@
 def is_prime(p: int) -> bool:
     """
-    Checks the number P for simplicity using finding the
-    remainder of the division in the range [2, P).
+    Checks the number P for simplicity using finding the remainder of the division in the range [2, P).
+    >>> is_prime(42)
+    False
+    >>> is_prime(73)
+    True
     """
-    a = 0
     for i in range(2, p):
         if p % i == 0:
             return False
     return True
 
 
-is_prime(42)
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=True)# или () без вывода на экран
+    
