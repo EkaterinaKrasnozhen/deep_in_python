0dce414ddc75e1fe5f1e1d32549b86d0c50b9d6c
kate <01redcat@mail.ru>
2023-07-23 12:16:07 +0300

    unitest

---
+++ Lection14/ex_doc_unitest.py
@@ -0,0 +1,12 @@
+import doctest
+import unittest
+import prime
+
+def load_tests(loader, tests, ignore):
+    tests.addTests(doctest.DocTestSuite(prime))
+    tests.addTests(doctest.DocFileSuite('prime.md'))
+    return tests
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=True)
