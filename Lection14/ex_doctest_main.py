410af1743e9419db1ccec1f935405ce6e2bc8bcf
kate <01redcat@mail.ru>
2023-07-23 12:01:17 +0300

    unitest

--- Lection14/ex_doctest_main.py
+++ Lection14/ex_doctest_main.py
@@ -1,3 +1,8 @@
 import doctest
 
 doctest.testfile('prime.md', verbose=True)
+
+# PS C:\Users\PycharmProjects> python -m doctest .\prime.py
+# PS C:\Users\PycharmProjects> python -m doctest .\prime.py -v
+# PS C:\Users\PycharmProjects> python -m doctest .\prime.md
+# PS C:\Users\PycharmProjects> python -m doctest .\prime.md -v
